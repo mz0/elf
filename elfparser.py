462c7ef0946c0c08ed37588499747d9294f70285
ArcherCreat <archercreat@gmail.com>
2018-11-17 00:43:23 +0300

    added program_header

--- elfparser.py
+++ elfparser.py
@@ -5,6 +5,16 @@ class elfparser:
         self._filename = filename
         self.elf = self._open(self._filename)
 
+    def _open(self, filename):
+        with open(filename, 'rb') as f:
+            f = f.read()
+            # 0x7F followed by ELF(45 4c 46)
+            if f[:0x4] != bytes(b'\x7f\x45\x4c\x46'):
+                raise ValueError('Not an elf file')
+            return f
+        raise ValueError(f'No file named "{filename}" was found')
+
+    # elf header is always on the same place
     def elf_header(self):
         self.EI_CLASS   = self.elf[0x4]
         self.EI_DATA    = self.elf[0x5]
@@ -41,18 +51,35 @@ class elfparser:
             # call to func to parse unknown endianness
             pass
 
+    def program_header(self):
+        # The program header table tells the system how to create a process image.
+        # It is found at file offset e_phoff, and consists of e_phnum entries,
+        # each with size e_phentsize.
+        # The layout is slightly different in 32-bit ELF vs 64-bit ELF,
+        # because the p_flags are in a different structure location
+        # for alignment reasons.
+        self.p_type = self.elf[self.e_phoff+0x00:self.e_phoff+0x04]
+        if self.EI_DATA == 1:
+            # p_flags are skipped in 32bit systems
+            self.p_offset   = self.elf[self.e_phoff+0x04:self.e_phoff+0x08]
+            self.p_vaddr    = self.elf[self.e_phoff+0x08:self.e_phoff+0x0C]
+            self.p_paddr    = self.elf[self.e_phoff+0x0C:self.e_phoff+0x10]
+            self.p_filesz   = self.elf[self.e_phoff+0x10:self.e_phoff+0x14]
+            self.p_memsz    = self.elf[self.e_phoff+0x14:self.e_phoff+0x18]
+            self.p_flags    = self.elf[self.e_phoff+0x18:self.e_phoff+0x1C]
+            self.p_align    = self.elf[self.e_phoff+0x1C:self.e_phoff+0x20]
+        elif self.EI_DATA == 2:
+            self.p_flags    = self.elf[self.e_phoff+0x04:self.e_phoff+0x08]
+            self.p_offset   = self.elf[self.e_phoff+0x08:self.e_phoff+0x10]
+            self.p_vaddr    = self.elf[self.e_phoff+0x10:self.e_phoff+0x18]
+            self.p_paddr    = self.elf[self.e_phoff+0x18:self.e_phoff+0x20]
+            self.p_filesz   = self.elf[self.e_phoff+0x20:self.e_phoff+0x28]
+            self.p_memsz    = self.elf[self.e_phoff+0x28:self.e_phoff+0x30]
+            self.p_align    = self.elf[self.e_phoff+0x30:self.e_phoff+0x38]
 
 
 
 
-    def _open(self, filename):
-        with open(filename, 'rb') as f:
-            f = f.read()
-            # 0x7F followed by ELF(45 4c 46)
-            if f[:0x4] != bytes(b'\x7f\x45\x4c\x46'):
-                raise ValueError('Not an elf file')
-            return f
-        raise ValueError(f'No file named {filename} was found')
 
 
 if __name__ == '__main__':
