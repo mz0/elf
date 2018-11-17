f83d2f5709e32873223c8e374d8f921d7415d988
ArcherCreat <archercreat@gmail.com>
2018-11-17 20:57:32 +0300

    Initial commit

---
+++ elfstruct.py
@@ -0,0 +1,6 @@
+from ctypes import *
+class elfstruct:
+    def __init__(self, little_endian=True, elfclass=32):
+        assert elfclass == 32 or elfclass == 64
+        self.little_endian = little_endian
+        self.elfclass = elfclass
